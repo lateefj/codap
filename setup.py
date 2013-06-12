37cb02e5484bed3e38a9cfbb530a1f58ec4786be
Lateef Jackson <lateef.jackson@gmail.com>
2013-06-12 10:43:43 -0400

    Converting to pypi project

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 DESCRIPTION = """Coroutine Data Access Patterns (mouthful I know) are some data
 access patterns I found useful building web applications that use gevent or
@@ -12,7 +17,7 @@ setup(
     license = 'BSD',
     keywords = 'gevent eventlet coroutine ',
     url = 'https://github.com/lateefj/codap',
-    packages=['codap'],
+    packages=['codap', 'tests'],
     long_description=read('README.md'),
     classifiers=[
             'Development Status :: 3 - Alpha',
