d0da17bdfb8c5830058a4ea69ccb2a6ce8551c31
Lateef Jackson <lateef.jackson@gmail.com>
2013-06-25 22:13:55 -0400

    Remove wrong docs

--- examples/fib_example.py
+++ examples/fib_example.py
@@ -19,12 +19,10 @@ for i in range(0, FIB_SIZE):
 for i in range(0, FIB_SIZE):
     assert d[i] == fib(i), 'Expected fib {0} to be {1} but was {3}'.format(i, d[i], fib(i))
 
-# Add in an order and reverse pull them out
 d = codap.Ordered()
 for i in range(0, FIB_SIZE):
     d.push(fib, i)
 
-# Now reverse order collect them out again
 i = 0
 for f in d:
     assert f == fib(i), 'Expected fib {0} to be {1} but was {3}'.format(i, f, fib(i))
